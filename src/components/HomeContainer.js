8bb2386549cc1aeea0e5acd7c8ca08673db9ec1d
barrcarballo <136733387+barrcarballo@users.noreply.github.com>
2025-08-21 20:26:53 -0300

    Revert "footer"

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import Services from '@/components/Services'
-import Footer from '@/components/Footer'
-
 import Hero from './Hero'
 
 const HomeContainer = () => {
   return (
-    <div>
-
-        <Hero/>
-
-    <Services />
-    <Footer />
-    </div>
+    <Hero />
   )
 }
 
