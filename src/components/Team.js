61fa3920a400b407c31dd5dd62099d6ad05f7692
Micaela Svatzky <svatzkymicaela@gmail.com>
2025-08-21 21:00:04 -0300

    added var colors

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,19 +1,20 @@
 import React from "react";
-import ServiceCard from "./TeamCard";
+import TeamCard from "@/components/TeamCard";
 
-const Services = () => {
+const Team = () => {
   return (
  
-      <div className="bg-yellow-200 text-black h-[600px] flex flex-col items-center justify-start pt-10 ">
+      <div className="bg-[var(--amarillo)] text-black h-[600px] flex flex-col items-center justify-start pt-10 ">
         <h1 className="text-4xl mb-2">The Team</h1>
         <p className="text-lg">Conocé a las capas cracks ídolas de nuestras diseñadoras :P</p>
         <div className="h-[250px] w-[1000px] flex justify-around mt-10 ">
-          <TeamCard image="/dummy/perrito.jpg" color="bg-sky-200" name="Renata Mandelman" role="Backend Developer" />
-          <TeamCard image="/dummy/perrito.jpg" color="bg-violet-400" name="Bárbara Carballo" role="UX/UI Designer"/>
-          <TeamCard image="/dummy/perrito.jpg" color="bg-orange-400" name="Micaela Svatzky" role="Frontend Developer"/>
+          <TeamCard image="/dummy/perrito.jpg" color="[var(--celeste)]" name="Renata Mandelman" role="Backend Developer" />
+          <TeamCard image="/dummy/perrito.jpg" color="[var(--violeta)]" name="Bárbara Carballo" role="UX/UI Designer"/>
+          <TeamCard image="/dummy/perrito.jpg" color="[var(--naranja)]" name="Micaela Svatzky" role="Frontend Developer"/>
         </div>
       </div>
     
   );
 };
+
 export default Team;
--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const ServiceCard = ({ image, color, name, role }) => {
+const TeamCard = ({ image, color, name, role }) => {
   return (
-    <div>
-      <div className={`flex flex-col justify-center items-center text-black  ${color} h-[60px] rounded-t-2xl`}>
+    <div className="hover:transition hover:shadow-2xl shadow-black">
+      <div className={`flex flex-col justify-center items-center text-black  bg-${color} h-[60px] rounded-t-2xl`}>
         <h5 className="font-semibold">{name}</h5>
         <p className="font-medium">{role}</p>
       </div>
