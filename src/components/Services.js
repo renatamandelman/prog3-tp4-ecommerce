fc07e003a1d33834904226de88055703f552d156
Micaela Svatzky <svatzkymicaela@gmail.com>
2025-09-04 18:53:05 -0300

    added width and svg

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,50 +9,51 @@ const Services = () => {
 
       <div className="relative w-full h-[500px]">
         <div>
-          <p className="text-black absolute left-264 top-5 text-2xl">
+          <p className="text-black absolute left-300 top-5 text-2xl">
             Web Design
           </p>
           <Image
-            src="/orangeTab.png"
-            width={1400}
+            src="/orangeTabSVG.svg"
+            width={1600}
             height={100}
             alt="orange tab"
+            className="w-[1800px]"
           />
         </div>
         <div>
-          <p className="text-white absolute left-132 top-32 text-2xl z-2">
+          <p className="text-white absolute left-150 top-32 text-2xl z-2">
             Branding
           </p>
           <Image
-            src="/purpleTab.png"
-            width={1400}
+            src="/purpleTabSVG.svg"
+            width={1600}
             height={100}
             alt="orange tab"
-            className="absolute top-25"
+            className="absolute top-25 w-[1800px]"
           />
         </div>
         <div>
-          <p className="text-black absolute left-230 top-60 text-2xl z-2">
+          <p className="text-black absolute left-260 top-60 text-2xl z-2">
             UX/UI
           </p>
           <Image
-            src="/blueTab.png"
-            width={1400}
+            src="/blueTabSVG.svg"
+            width={1600}
             height={100}
             alt="orange tab"
-            className="absolute top-55"
+            className="absolute top-55 w-[1800px]"
           />
         </div>
         <div>
-             <p className="text-black absolute left-68 top-85 text-2xl z-2">
+             <p className="text-black absolute left-77 top-85 text-2xl z-2">
             Our Products
           </p>
         <Image
-          src="/yellowTab.png"
-          width={1400}
+          src="/yellowTabSVG.svg"
+          width={1600}
           height={100}
           alt="orange tab"
-          className="absolute top-80"
+          className="absolute top-80 w-[1800px]"
         /></div>
       </div>
     </div>
