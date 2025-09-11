3ae6a996cae19e125e605cb9aaa82c485e56f18d
valere mandel <valeremandel@renata-Macbook-Pro.local>
2025-09-11 15:09:35 -0300

    feature/ourProducts

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import HomeContainer from "@/components/HomeContainer";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div  >
+          <Navbar /> 
       <HomeContainer />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="absolute w-full h-[50px] text-[#3B413C] px-4 flex flex-row items-center justify-between ">
-      <Image
+    <Link href="">  <Image
         src="/dummy/perrito.jpg"
         width={40}
         height={40}
         alt="perrito cool"
       />
-
+</Link>
       <ul className="flex flex-row w-[40%] justify-between font-bold mr-4 pr-2">
-        <li className="flex flex-row items-center"><a href='/services'>Services</a> <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M6 18L18 6m0 0H9m9 0v9"></path></svg></li>
-        <li><a href=''>Our Products</a></li>
-        <li><a href=''>About Us</a></li>
-        <li><a href=''>Contact Us</a></li>
+        <li className="flex flex-row items-center"><Link href='/services'>Services</Link> <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M6 18L18 6m0 0H9m9 0v9"></path></svg></li>
+        <li><Link href='/ourProducts'>Our Products</Link></li>
+        <li><Link href=''>About Us</Link></li>
+        <li><Link href=''>Contact Us</Link></li>
       </ul>
     </div>
   );
