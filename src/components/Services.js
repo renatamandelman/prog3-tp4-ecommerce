9641a8dd86547ebe4ac143517b52c8f9de894137
renatamandelman <101489121+renatamandelman@users.noreply.github.com>
2025-09-18 20:22:39 -0300

    Revert "Develop"

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,53 +9,51 @@ const Services = () => {
 
       <div className="relative w-full h-[500px]">
         <div>
-          <p className="text-black absolute left-300 top-5 text-2xl">
+          <p className="text-black absolute left-264 top-5 text-2xl">
             Web Design
           </p>
           <Image
-            src="/orangeTabSVG.svg"
-            width={1600}
+            src="/orangeTab.png"
+            width={1400}
             height={100}
-            alt="orange tab"
-            className="w-[1800px]"
+            alt="orange tab w-[1400px]"
           />
         </div>
         <div>
-          <p className="text-white absolute left-150 top-32 text-2xl z-2">
+          <p className="text-white absolute left-132 top-32 text-2xl z-2">
             Branding
           </p>
           <Image
-            src="/purpleTabSVG.svg"
-            width={1600}
+            src="/purpleTab.png"
+            width={1400}
             height={100}
-            alt="orange tab"
-            className="absolute top-25 w-[1800px]"
+            alt="purple tab"
+            className="absolute top-25 w-[1400px]"
           />
         </div>
         <div>
-          <p className="text-black absolute left-260 top-60 text-2xl z-2">
+          <p className="text-black absolute left-230 top-60 text-2xl z-2">
             UX/UI
           </p>
           <Image
-            src="/blueTabSVG.svg"
-            width={1600}
+            src="/blueTab.png"
+            width={1400}
             height={100}
-            alt="orange tab"
-            className="absolute top-55 w-[1800px]"
+            alt="blue tab"
+            className="absolute top-55 w-[1400px]"
           />
         </div>
         <div>
-          <p className="text-black absolute left-77 top-85 text-2xl z-2">
+             <p className="text-black absolute left-68 top-85 text-2xl z-4">
             Our Products
           </p>
-          <Image
-            src="/yellowTabSVG.svg"
-            width={1600}
-            height={100}
-            alt="orange tab"
-            className="absolute top-80 w-[1800px]"
-          />
-        </div>
+        <Image
+          src="/yellowTab.png"
+          width={1400}
+          height={100}
+          alt="yellow tab"
+          className="absolute top-80 w-[1400px] z-3"
+        /></div>
       </div>
     </div>
   );
