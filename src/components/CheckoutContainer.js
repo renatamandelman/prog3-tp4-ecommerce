e3b4bebd113fa74420a0acd373cb4fa39ce00cd3
barrcarballo <barbie.carb@gmail.com>
2025-10-16 20:19:12 -0300

    checkout-ui2

--- src/components/CheckoutContainer.js
+++ src/components/CheckoutContainer.js
@@ -30,13 +30,13 @@ const CheckoutContainer = () => {
     return (
       <div className="bg-[#e6d8c7] min-h-screen flex items-center justify-center font-sans">
         <div className="text-center bg-white p-12 rounded-lg shadow-md">
-          <h1 className="text-2xl font-semibold text-gray-700">
-            Tu carrito está vacío
+          <h1 className="text-2xl font-semibold text-gray-700 flex justify-center items-center gap-0.5">
+            Tu carrito está vacío <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 36 36"><path fill="#001" d="M18 2a16 16 0 1 0 16 16A16 16 0 0 0 18 2Zm0 30a14 14 0 1 1 14-14a14 14 0 0 1-14 14Z" className="clr-i-outline clr-i-outline-path-1"/><circle cx="25.16" cy="14.28" r="1.8" fill="#001" className="clr-i-outline clr-i-outline-path-2"/><circle cx="11.41" cy="14.28" r="1.8" fill="#001" className="clr-i-outline clr-i-outline-path-3"/><path fill="#001" d="M18.16 20a9 9 0 0 0-7.33 3.78a1 1 0 1 0 1.63 1.16a7 7 0 0 1 11.31-.13a1 1 0 0 0 1.6-1.2A9 9 0 0 0 18.16 20Z" className="clr-i-outline clr-i-outline-path-4"/><path fill="none" d="M0 0h36v36H0z"/></svg>
           </h1>
-          <p className="text-gray-500 mt-2">
-            No hay productos para mostrar en el checkout.
+          <p className="text-gray-500 mt-2 mb-7">
+            No hay productos para mostrar en el checkout. 
           </p>
-          <Link href="/ourProducts" className="mt-12 bg-indigo-600 text-white font-bold py-3 px-6 rounded-lg hover:bg-indigo-700 transition">
+          <Link href="/ourProducts" className="bg-[var(--verde-oscuro)] font-bold py-3 px-6 rounded-lg hover:bg-[var(--naranja)] transition">
             Volver a la tienda
           </Link>
         </div>
